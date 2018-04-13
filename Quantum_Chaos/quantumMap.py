a1f0d37dcea2f36940512d4c4784c2cf9ab81bc2
AlGepe <alvarodiezgp@gmail.com>
2018-04-13 15:55:54 +0200

    Let's find the mistake for k != 0

--- Quantum_Chaos/quantumMap.py
+++ Quantum_Chaos/quantumMap.py
@@ -5,9 +5,6 @@ import matplotlib.pyplot as plt
 
 def prepGauss(M, x0, p0):
     # Define characteristics of WavePacket
-    M = 100
-    x0 = 3
-    p0 = 1.1
     pi = np.pi
     # Convert to n, m
     n0 = M * x0 / (2 * pi)
@@ -32,7 +29,8 @@ def prepGauss(M, x0, p0):
     plt.ylabel('Probability Density')
     plt.xlabel('x_n')
     plt.legend(loc=1)
-    plt.show()
+    '''
+    # plt.show()
     psi_Gm = fft(psi_Gn) / np.sqrt(M)
     plt.clf()
     probDens_m = psi_Gm * np.conj(psi_Gm)
@@ -43,13 +41,45 @@ def prepGauss(M, x0, p0):
     plt.ylabel('Probability Density')
     plt.xlabel('p_n')
     plt.legend(loc=1)
-    plt.show()
+    # plt.show()
+    '''
     return psi_Gn
 
 
-def oneStep(psi):
-
+def oneStepFft(psi, K):
+    # print(type(psi[0]))
+    pi = np.pi
+    M = len(psi)
+    n = np.arange(M)
+    bigP = np.exp((1j*np.square(n)*pi)/M)
+    bigV = np.exp(-1j*M*K*np.cos((2*pi*n)/M)/2*pi)
+    psi = (bigP * fft(bigP*bigV*psi)) / np.sqrt(M)
     return psi
 
 
-prepGauss(3, 3, 1.1)
+def doIt(M, x0, p0, K, t_steps):
+    pi = np.pi
+    x_n = (np.arange(M) * 2 * pi) / M
+    init = prepGauss(M, x0, p0)
+    psi = np.zeros(shape=(t_steps, len(init)), dtype=complex)
+    psi[0] = init
+    plt.clf()
+    plt.plot(x_n, psi[0]*np.conj(psi[0]), 'b+-')
+    plt.axis([0, 2*pi, 0, 0.15])
+    plt.savefig("Results/wave_packet" + str(0).zfill(3) + ".png")
+    for t in range(t_steps-1):
+        plt.clf()
+        psi[t+1] = oneStepFft(psi[t], K)
+        plt.plot(x_n, psi[t+1] * np.conj(psi[t+1]), 'b+-')
+        plt.axis([0, 2*pi, 0, 0.15])
+        plt.savefig("Results/wave_packet" + str(t).zfill(3) + ".png")
+        # plt.show()
+
+
+doIt(100, 3, 0.01, 2.1, 20)
+
+
+
+
+
+
