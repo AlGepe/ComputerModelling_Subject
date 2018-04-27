2d90e9df160c3edc491715e6ca776640396d78d7
AlGepe <alvarodiezgp@gmail.com>
2018-04-27 15:13:47 +0200

    Another unfinished bunsiness

--- Prisoners_Dilemma/prisonersMain.py
+++ Prisoners_Dilemma/prisonersMain.py
@@ -1,12 +1,15 @@
 import numpy as np
+import utils as u
 # import matplotlib
 # matplotlib.use('pdf')
 import matplotlib.pyplot as plt
 
 
-decisionGrid = np.zeros(shape=(21, 21))
+decisionGrid = np.zeros(shape=(22, 22))
 decisionGrid[10][10] = 1
 
-plt.imshow(decisionGrid, interpolation='nearest', cmap=plt.cm.gra_r)
-plt.show()
-
+for i in range(20):
+    payOff = u.getPayOff(decisionGrid, 3)
+    decisionGrid = u.updateDecisionGrid(decisionGrid, payOff)
+    plt.imshow(decisionGrid, interpolation='nearest', cmap=plt.cm.gray_r)
+    plt.show()
--- Prisoners_Dilemma/utils.py
+++ Prisoners_Dilemma/utils.py
@@ -3,7 +3,7 @@ import numpy as np
 #              Cooperate == 1
 
 
-def getPayOff(decisionGrid, reward):
+def getPayOff_loop(decisionGrid, reward):
     dim = decisionGrid.shape
     payOff_grid = np.zeros(dim)
     for i in range(dim[0]):
@@ -23,13 +23,55 @@ def getPayOff(decisionGrid, reward):
 def updateDecisionGrid(oldDecision, payOff_grid):
     dim = oldDecision.shape
     newDecision = np.zeros(dim)
-    for i in range(dim[0]):
-        for j in range(dim[1]):
-            neighbours = oldDecision[i-1:i+1][j-1:j+1]
+    for i in range(1, dim[0]-1):
+        for j in range(1, dim[1]-1):
+            neighbours = oldDecision[i-1:i+1, j-1:j+1]
+            #print(neighbours)
             winnerPos = np.unravel_index(neighbours.argmax(), neighbours.shape)
             newDecision[i][j] = oldDecision[i-1 + winnerPos[0]][j-1+winnerPos[1]]
 
     return newDecision
 
+
+def getPayOff(decisionGrid, reward):
+    # sum == how many neighbours did C
+    b = 8
+    sumGrid = (decisionGrid +
+               shiftPB(decisionGrid, [ 1,  1]) +
+               shiftPB(decisionGrid, [ 1,  0]) + 
+               shiftPB(decisionGrid, [ 1, -1]) +
+               shiftPB(decisionGrid, [ 0,  1]) +
+               shiftPB(decisionGrid, [ 0, -1]) +
+               shiftPB(decisionGrid, [-1, 1]) +
+               shiftPB(decisionGrid, [-1, 0]) +
+               shiftPB(decisionGrid, [-1, -1]))
+    payOff = sumGrid * decisionGrid  # CC = +1
+    payOff += (decisionGrid == 0) * ((sumGrid) * b)  # DC = b
+    return payOff
+
+
+def shiftPB(original, shift):
+    shifted = np.zeros(original.shape)
+    i = shift[0]
+    j = shift[1]
+    if j == 0:
+        shifted[i:, :] = original[:-i, :]
+        # shifted[i:, :] = original[i:, -j:]
+        shifted[:i, j:] = original[-i:, j:]
+        #shifted[:i, :] = original[-i:, -j:]
+
+    elif i == 0:
+        shifted[i:, j:] = original[:, :-j]
+        shifted[i:, :j] = original[i:, -j:]
+        # shifted[:i, j:] = original[-i:, j:]
+        #shifted[:i, :j] = original[-i:, -j:]
+
+    else:
+        shifted[i:, j:] = original[:-i, :-j]
+        shifted[i:, :j] = original[i:, -j:]
+        shifted[:i, j:] = original[-i:, j:]
+        shifted[:i, :j] = original[-i:, -j:]
+    return shifted
+    
 
 
