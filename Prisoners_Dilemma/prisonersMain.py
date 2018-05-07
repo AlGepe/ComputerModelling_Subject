9dc13dbc86f61dd3db27bd1879301e99495906bf
AlGepe <alvarodiezgp@gmail.com>
2018-05-07 20:50:35 +0200

    Fixed momentum issue but doesn't seem right yet

--- Prisoners_Dilemma/prisonersMain.py
+++ Prisoners_Dilemma/prisonersMain.py
@@ -5,21 +5,23 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 
-sizeHalf = 10
+sizeHalf = 100
 N = (sizeHalf * 2) + 1
 decisionGrid = np.zeros(shape=(N, N))
-decisionGrid[sizeHalf-1][sizeHalf-1] = 1
+decisionGrid[sizeHalf][sizeHalf] = 1
 
 for i in range(200):
     namePO = "Pay_Off_grid_" + str(i)
     nameDec = "Decision_grid_" + str(i)
-    payOff = u.getPayOff(decisionGrid, 2.9)
+    payOff = u.getPayOff(decisionGrid, 1.9)
+    # print(payOff)
     # print(decisionGrid.shape)
     decisionGrid = u.updateDecisionGrid(decisionGrid, payOff)
     # print(decisionGrid.shape)
     plt.imshow(payOff, interpolation='nearest', cmap=plt.cm.gray_r)
+    plt.show()
     # plt.show()
     # plt.savefig(namePO + '.png')
     plt.imshow(decisionGrid, interpolation='nearest', cmap=plt.cm.gray_r)
-    plt.savefig(nameDec + '.png')
     plt.show()
+    plt.savefig(nameDec + '.png')
--- Prisoners_Dilemma/utils.py
+++ Prisoners_Dilemma/utils.py
@@ -36,9 +36,12 @@ def updateDecisionGrid(oldDecision, payOff_grid):
     newDecision = np.zeros(oldDecisionPB.shape)
     for i in range(1, dim[0]+1):
         for j in range(1, dim[1]+1):
-            neighbours = oldDecisionPB[i-1:i+1, j-1:j+1]
+            neighbours = oldDecisionPB[i-1:i+2, j-1:j+2]
+            # print(neighbours)
             # print(neighbours)
             winnerPos = np.unravel_index(neighbours.argmax(), neighbours.shape)
+            # print(neighbours[winnerPos[0], winnerPos[1]])
+            # print(winnerPos)
             newDecision[i][j] = oldDecisionPB[i-1 + winnerPos[0]][j-1+winnerPos[1]]
 
     # print("old Decision: {0}".format(oldDecision.shape))
@@ -46,6 +49,8 @@ def updateDecisionGrid(oldDecision, payOff_grid):
     # plt.imshow(newDecision, interpolation='nearest', cmap=plt.cm.gray_r)
     # plt.title("New Decision")
     # plt.show()
+    # print(newDecision)
+    # print(newDecision[1:dim[0] + 1, 1:dim[1] + 1])
     return newDecision[1:dim[0] + 1, 1:dim[1] + 1]
 
 
