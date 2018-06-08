586fb22731200c273887bfdf5e1ab5bfda80c1e3
AlGepe <alvarodiezgp@gmail.com>
2018-06-08 11:35:00 +0200

    Weights not normalized

--- Networks/karateMain.py
+++ Networks/karateMain.py
@@ -1,6 +1,7 @@
 import networkx as nx
 from pylab import *
 import numpy as np
+import math
 
 def showNet(g):
     pos = nx.spring_layout(g)
@@ -12,15 +13,57 @@ def showNet(g):
     nx.draw_networkx_labels(g, pos, labels, font_zise=12)
     show()
 
+def f(x):
+    print(x)
+    f = math.pow(x-0.25, 3)
+    print(f)
+    print('\n')
+    return f
+
+
+def  doTimeStep(g, dt):
+    # c = D * sum(c_rest - c) * w_edge
+    newG = g.copy()
+    nx.set_node_attributes(newG, 0, 'state')
+    nx.set_edge_attributes(newG, 0, 'weight')
+    beta = 10
+    D = 5
+    # Update Nodes
+    valuesList = np.zeros(len(g.nodes()), dtype=float)
+    for node in g.nodes():
+        print(node)
+        formula = 0
+        for neighbour in g.neighbors(node):
+            oneEdge = (node, neighbour)
+            formula += (g.node[node]['state'] - g.node[neighbour]['state'])  *\
+                        g.edges()[oneEdge]['weight']
+        valuesList[node] = (g.node[node]['state'] + D * dt * formula)
+    valuesList[0] = 1.
+    valuesList[33] = .0
+    valuesList = abs((valuesList-min(valuesList)) / float(max(valuesList)))
+    valuesDict = {}
+    for node in g.nodes():
+        valuesDict[node] = {'state' : valuesList[node]}
+
+    nx.set_node_attributes(newG, valuesDict)
+    # Update Weights
+    valuesList = 0
+    for i in g.edges():
+        w_i = g.edges()[i]['weight']
+        f_value = f(abs(g.node[i[0]]['state'] - g.node[i[1]]['state']))
+        newG.edges()[i]['weight'] = w_i- beta*w_i*(1-w_i)*dt  * f_value
+        newG.edges()[i]['weight'] = w_i- beta*w_i*(1-w_i)*dt  * f_value
+
+    return newG
+
+
 g = nx.karate_club_graph()
-states = {}
-for i in range(len(g)):
-    #states[i] = np.random.ranf()
-    states[i] = .5
-nx.set_node_attributes(g, states, 'state')
+nx.set_node_attributes(g, 0.5, 'state')
 nx.set_edge_attributes(g, 0.5,'weight')
-g.node[0]['state'] = 1
-g.node[len(g)-1]['state'] = 0
+g.node[0]['state'] = 1.
+g.node[len(g)-1]['state'] = .0
+print(g.node[0])
+print(g.node[33])
 
 
 # /Initialise
@@ -30,5 +73,14 @@ g.node[len(g)-1]['state'] = 0
 # Iterations
     # solve ODE for clubs
     # solver ODE for edges
-nx.draw_spring(g, cmap = cm.cool, vmin = 0, vmax = 1,with_labels = True, node_color = [g.node[i]['state'] for i in g.nodes()], edge_cmap = cm.binary, edge_vmin = 0, edge_vmax = 1,edge_color = [g[i][j]['weight'] for i, j in g.edges])
-show()
+for i in range(100):
+    g = doTimeStep(g, 0.01)
+    if not(i%10):
+        print(g.nodes(data=True))
+        print(g.edges(data=True))
+        nx.draw_spring(g, cmap = cm.cool, vmin = 0, vmax = 1,
+                        with_labels = True,
+                        node_color = [g.node[i]['state'] for i in g.nodes()],
+                        edge_cmap = cm.binary, edge_vmin = 0, edge_vmax = 1,
+                        edge_color = [g[i][j]['weight'] for i, j in g.edges])
+        show()
