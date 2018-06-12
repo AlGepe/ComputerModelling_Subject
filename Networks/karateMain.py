360586d520d15f5cd01a0b54804df190c49d46c7
AlGepe <alvarodiezgp@gmail.com>
2018-06-12 19:59:42 +0200

    Seems to be done

--- Networks/karateMain.py
+++ Networks/karateMain.py
@@ -14,12 +14,51 @@ def showNet(g):
     show()
 
 def f(x):
-    print(x)
+    # print(x)
     f = math.pow(x-0.25, 3)
-    print(f)
-    print('\n')
+    # print(f)
+    # print('\n')
     return f
 
+def zacharyData():
+    data = np.array([[0, 4, 5, 3, 3, 3, 3, 2, 2, 0, 2, 3, 2, 3, 0, 0, 0, 2, 0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0],
+                     [4, 0, 6, 3, 0, 0, 0, 4, 0, 0, 0, 0, 0, 5, 0, 0, 0, 1, 0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0],
+                     [5, 6, 0, 3, 0, 0, 0, 4, 5, 1, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 3, 0],
+                     [3, 3, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [3, 0, 0, 0, 0, 0, 2, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [3, 0, 0, 0, 0, 0, 5, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [3, 0, 0, 0, 2, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [2, 4, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [2, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 4, 3],
+                     [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+                     [2, 0, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [1, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [3, 5, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 4],
+                     [0, 0, 0, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2],
+                     [2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 1],
+                     [2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 4, 0, 2, 0, 0, 5, 4],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 3, 0, 0, 0, 2, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 2, 0, 0, 0, 0, 0, 0, 7, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 2],
+                     [0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+                     [0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 4, 0, 0, 0, 0, 0, 3, 2],
+                     [0, 2, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3],
+                     [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 7, 0, 0, 2, 0, 0, 0, 4, 4],
+                     [0, 0, 2, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3, 3, 0, 0, 1, 0, 3, 0, 2, 5, 0, 0, 0, 0, 0, 4, 3, 4, 0, 5],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 4, 2, 0, 0, 0, 3, 2, 4, 0, 0, 2, 1, 1, 0, 3, 4, 0, 0, 2, 4, 2, 2, 3, 4, 5, 0]])
+    print(data)
+    oneMax = np.amax(data)
+    thaMax = np.amax(oneMax)
+    return data/thaMax
 
 def  doTimeStep(g, dt):
     # c = D * sum(c_rest - c) * w_edge
@@ -31,39 +70,64 @@ def  doTimeStep(g, dt):
     # Update Nodes
     valuesList = np.zeros(len(g.nodes()), dtype=float)
     for node in g.nodes():
-        print(node)
-        formula = 0
+        # print(node)
+        theSum = 0
         for neighbour in g.neighbors(node):
             oneEdge = (node, neighbour)
-            formula += (g.node[node]['state'] - g.node[neighbour]['state'])  *\
-                        g.edges()[oneEdge]['weight']
-        valuesList[node] = (g.node[node]['state'] + D * dt * formula)
+            theSum += (g.node[neighbour]['state'] - g.node[node]['state']) * g.edges()[oneEdge]['weight']
+        valuesList[node] = (g.node[node]['state'] + D * dt * theSum)
     valuesList[0] = 1.
-    valuesList[33] = .0
-    valuesList = abs((valuesList-min(valuesList)) / float(max(valuesList)))
+    valuesList[-1] = .0
+    valuesList = abs((valuesList-min(valuesList)) / float(abs(max(valuesList)-min(valuesList))))
     valuesDict = {}
+    valuesList[0] = 1.
+    valuesList[-1] = .0
     for node in g.nodes():
         valuesDict[node] = {'state' : valuesList[node]}
 
     nx.set_node_attributes(newG, valuesDict)
     # Update Weights
-    valuesList = 0
+    valuesList = np.zeros(len(g.edges()))
+    j = 0
     for i in g.edges():
         w_i = g.edges()[i]['weight']
         f_value = f(abs(g.node[i[0]]['state'] - g.node[i[1]]['state']))
+        valuesList[j] = w_i- beta*w_i*(1-w_i)*dt  * f_value
         newG.edges()[i]['weight'] = w_i- beta*w_i*(1-w_i)*dt  * f_value
-        newG.edges()[i]['weight'] = w_i- beta*w_i*(1-w_i)*dt  * f_value
+        j += 1
 
     return newG
 
+def stdInitCond(g):
+    nx.set_node_attributes(g, 0.5, 'state')
+    nx.set_edge_attributes(g, 0.5, 'weight')
+    g.node[0]['state'] = 1.
+    g.node[len(g)-1]['state'] = .0
+    return g
+
+def zacharyCondt(g):
+    data = zacharyData()
+    print(data)
+    nx.set_node_attributes(g, 0.5, 'state')
+    nx.set_edge_attributes(g, 0.5, 'weight')
+    g.node[0]['state'] = 1.
+    g.node[len(g)-1]['state'] = .0
+    dictEdges= {}
+    for i in range(len(data)):
+        for j in range(i, len(data[0])):
+            dictEdges[(i, j)] = {'weight' : data[i, j]}
+    nx.set_edge_attributes(g, dictEdges)
+    return g
+
+
+
+
 
 g = nx.karate_club_graph()
-nx.set_node_attributes(g, 0.5, 'state')
-nx.set_edge_attributes(g, 0.5,'weight')
-g.node[0]['state'] = 1.
-g.node[len(g)-1]['state'] = .0
-print(g.node[0])
-print(g.node[33])
+g = zacharyCondt(g)
+# g = stdInitCond(g)
+# print(g.node[0])
+# print(g.node[33])
 
 
 # /Initialise
@@ -73,10 +137,10 @@ print(g.node[33])
 # Iterations
     # solve ODE for clubs
     # solver ODE for edges
-for i in range(100):
+for i in range(1000):
     g = doTimeStep(g, 0.01)
-    if not(i%10):
-        print(g.nodes(data=True))
+    if not(i%100):
+        # print(g.nodes(data=True))
         print(g.edges(data=True))
         nx.draw_spring(g, cmap = cm.cool, vmin = 0, vmax = 1,
                         with_labels = True,
